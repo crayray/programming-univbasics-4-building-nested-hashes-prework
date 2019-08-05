9f12d21abf2525c16512040abf0bb0df96b4999c
Rachel Fuerst <rach.fuerst@gmail.com>
2019-08-05 01:27:34 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -26,10 +26,13 @@ def monopoly_with_third_tier
 	hash = {
 		:railroads => {
 			:rent_in_dollars => {
-				names: "",
+				key1: "",
 				key2: "",
 				key3: "",
 				key4: ""
+			}
+			:names => {
+				
 			}
 		}
 	}
